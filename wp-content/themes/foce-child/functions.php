1df3ab4dba63355a613621218764e00e519c3be0
Noémie Koelblen <noemie.koelblen@icloud.com>
2023-12-30 19:12:25 +0100

    Swiper

--- wp-content/themes/foce-child/functions.php
+++ wp-content/themes/foce-child/functions.php
@@ -4,6 +4,7 @@ function theme_enqueue_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('sections-style', get_stylesheet_directory_uri() . '/assets/sass/sections.css');
+    wp_enqueue_style('swiper-css', get_stylesheet_directory_uri() . '/assets/swiper/swiper-bundle.min.css', array());
 }
 
 add_action('wp_enqueue_scripts', 'theme_enqueue_scripts');
@@ -11,6 +12,8 @@ function theme_enqueue_scripts()
 {
     wp_register_script('sections', get_theme_file_uri('/assets/js/sections.js'), '', false, true);
     wp_enqueue_script('sections');
+    wp_register_script('swiper-js', get_stylesheet_directory_uri() . '/assets/swiper/swiper-bundle.min.js', array(), '11.5.0', true);
+    wp_enqueue_script('swiper-js');
 }
 
 // Get customizer options form parent theme
